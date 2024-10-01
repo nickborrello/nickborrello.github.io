8449e370c2b5255ad8345fdd26303752495040b3
Nick <nvborrello@gmail.com>
2024-10-01 18:39:07 -0400

    Refactor project data: Update title for Auction House application

--- data/index.ts
+++ data/index.ts
@@ -51,7 +51,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building an Auction House application",
+    title: "Currently building an Auction House application.",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
